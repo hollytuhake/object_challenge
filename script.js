4ae3f2c8e5648d14856b49ad0e183cd343bf89f3
Holly Tuhake <hollytuhake@Nicoles-MacBook-Pro-2.local>
2017-10-03 19:45:36 -0500

    completed

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 var locations = []; // this array will hold your objects
 
 // object constructor
+var Region = function(state, revenue, locationCount){
+  this.state = state;
+  this.revenue = revenue;
+  this.locationCount = locationCount;
+  this.averageRevenue = revenue / locationCount;
+};
 
 // object instances
+var illinois = new Region ('IL', 5000, 12);
+var minnesota = new Region ('MN', 300, 3);
+var nevada = new Region ('NV', 25000, 1);
 
 // push object instances to locations array
+locations.push(illinois);
+locations.push(minnesota);
+locations.push(nevada);
+
 
 // DO NOT MODIFY THIS CODE
 $(document).ready(function () {
